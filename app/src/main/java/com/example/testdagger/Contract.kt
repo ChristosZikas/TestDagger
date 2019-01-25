49331f9716f940873676fc041fe6da8d47fd96a0
Christos Zikas <christos@ddm.health>
2019-01-25 09:24:53 +0000

    adding the contract

---
+++ app/src/main/java/com/example/testdagger/Contract.kt
@@ -0,0 +1,17 @@
+package com.example.testdagger
+
+interface Contract {
+
+    interface Foo {
+        fun onFooCmdLog(l: FooCmd)
+        fun onFooCmdPostBarCmd(l: FooCmd)
+        fun onFooCmdUpdateText(l: FooCmd)
+    }
+
+    interface Bar {
+        fun onBarCmdLog(o: BarCmd)
+        fun onBarCmdUpdateText(o: BarCmd)
+    }
+
+
+}
